967fb12ab69463ea1e276b3439570468a3d1c6fc
Alexander Karpachev <alexander.karpachev@live.ru>
2021-01-26 17:35:47 +1100

    Pygame 6 Платформы - готово

--- Pygame/PyGame06-06.py
+++ Pygame/PyGame06-06.py
@@ -7,78 +7,87 @@ WINDOW_SIZE = WINDOW_WIDTH, WINDOW_HEIGHT = 800, 600
 FPS = 30
 
 
-class Colors(enum.Enum):
+class GameColors(enum.Enum):
     hero = blue = pygame.Color('blue')
     platform = gray = pygame.Color('gray')
     bg = black = pygame.Color('black')
 
 
 class Hero(pygame.sprite.Sprite):
+    image = pygame.Surface((20, 20))
+
     def __init__(self, group):
         super().__init__(group)
         self.active = False
-        self.image = pygame.Surface(self.w, self.h)
         self.rect = self.image.get_rect()
-        pygame.draw.rect(self.image, Colors.hero.value, self.rect, 0)
+        pygame.draw.rect(self.image, GameColors.hero.value, self.rect, 0)
+        self.rect.x, self.rect.y = (-20, 0)
+        self.speed = 10.0
+        self.gravity = 50.0
 
-    def update(self, group, event, time):
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if not self.active:
-                self.active = True
-            self.rect.left, self.rect.top = event.pos
+    def respawn(self, pos):
+        if not self.active:
+            self.active = True
+        self.rect.x, self.rect.y = pos
+
+    def action(self, event):
         if not self.active:
             return
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                self.rect.left -= 10.0 * time
+                self.rect = self.rect.move(-self.speed, 0)
             elif event.key == pygame.K_RIGHT:
-                self.rect.left += 10.0 * time
+                self.rect = self.rect.move(self.speed, 0)
+
+    def update(self, group, time):
+        if not self.active:
+            return
         if not pygame.sprite.spritecollideany(self, group):
-            self.rect.top += round(10.0 * time)
-        #c = [b for b in pygame.sprite.spritecollide(self, gb, False) if b.id != self.id]
+            self.rect = self.rect.move(0, self.gravity * time)
 
 
 class Platform(pygame.sprite.Sprite):
-    # строго вертикальный или строго горизонтальный отрезок
-    def __init__(self, group, gv, event):
+    image = pygame.Surface((50, 10))
+
+    def __init__(self, group, gp, pos):
         super().__init__(group)
-        self.add(gv)
-        self.image = pygame.Surface((50, 10))
+        self.add(gp)  # отдельная группа для платформ
         self.rect = self.image.get_rect()
-        self.rect.left, self.rect.top = event.pos
-        pygame.draw.rect(self.image, Colors.platform.value, self.rect, 0)
-        print('Add platform')
+        pygame.draw.rect(self.image, GameColors.platform.value, self.rect, 0)
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
 
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode(WINDOW_SIZE)
-    bg = Colors.black.value
+    bg = GameColors.black.value
 
     all_sprites = pygame.sprite.Group()
     platforms = pygame.sprite.Group()
-
+    hero = Hero(all_sprites)
+    t = 0
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
                 break
-            elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
-                    Platform(all_sprites, platforms, event)
-                elif event.button == 2:
-                    t = clock.tick(FPS) / 1000.0
-                    all_sprites.update(all_sprites, platforms, event, t)
-
+                    Platform(all_sprites, platforms, event.pos)
+                if event.button == 3:
+                    hero.respawn(event.pos)
+            if event.type == pygame.MOUSEMOTION:
+                pygame.display.set_caption(f'{hero.active}')
+            hero.action(event)
         if not running:
             break
-
         screen.fill(bg)
-        clock.tick(FPS)
         all_sprites.draw(screen)
-        platforms.draw(screen)
+        all_sprites.update(platforms, t)
+        t = clock.tick(FPS) / 1000.0
         pygame.display.flip()
     pygame.display.quit()
 
