8d41d4a7d548dfb3a6f1384a4183ad75286770b2
Alexander Karpachev <alexander.karpachev@live.ru>
2021-02-01 23:52:24 +1100

    Pygame 7 Перемещение героя. Камера.

--- Pygame/PyGame07-03.py
+++ Pygame/PyGame07-03.py
@@ -5,7 +5,7 @@ import pygame
 
 pygame.init()
 
-WINDOW_SIZE = WINDOW_WIDTH, WINDOW_HEIGHT = 550, 550
+WINDOW_SIZE = WINDOW_WIDTH, WINDOW_HEIGHT = 300, 300
 screen = pygame.display.set_mode(WINDOW_SIZE)
 
 clock = pygame.time.Clock()
@@ -85,35 +85,41 @@ class Tile(pygame.sprite.Sprite):
     def __init__(self, tile_type, pos_x, pos_y):
         super().__init__(tiles_group, all_sprites)
         self.image = tile_images[tile_type]
-        self.rect = self.get_rect((pos_x, pos_y))
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
         self.type = tile_type
-        self.pos = (pos_x, pos_y)
+        self.orig_x = self.rect.x
+        self.orig_y = self.rect.y
 
-    def get_rect(self, pos):
-        x, y = pos
-        return self.image.get_rect().move(tile_width * x, tile_height * y)
+    def reset_pos(self):
+        self.rect.x = self.orig_x
+        self.rect.y = self.orig_y
 
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y):
-        super().__init__(player_group)
+        super().__init__(player_group, all_sprites)
         self.image = player_image
-        self.rect = self.image.get_rect().move(
-            tile_width * pos_x + 15, tile_height * pos_y + 5)
+        self.rect = self.getrect((pos_x, pos_y))
         self.pos = (pos_x, pos_y)
-        print(f'Player: {self.rect}')
+
+    def reset_pos(self):
+        pass
+
+    def getrect(self, pos):
+        return self.image.get_rect().move(
+            tile_width * pos[0] + 15, tile_height * pos[1] + 5)
 
     def move(self, pos):
-        old_pos = self.pos
-        old_rect = self.rect
-        self.pos = self.pos[0] + pos[0], self.pos[1] + pos[1]
-        self.rect = self.image.get_rect().move(
-            tile_width * self.pos[0] + 15, tile_height * self.pos[1] + 5)
+        new_pos = (self.pos[0] + pos[0], self.pos[1] + pos[1])
+        self.rect = self.getrect(new_pos)
+        for sprite in all_sprites:
+            sprite.reset_pos()
         c = pygame.sprite.groupcollide(player_group, tiles_group, False, False)
         if c[self][0].type == 'wall':
-            self.rect = old_rect
-            self.pos = old_pos
-        print(f'{old_rect}, {self.rect}')
+            self.rect = self.getrect(self.pos)
+        else:
+            self.pos = new_pos
 
     def action(self, event):
         if event.type == pygame.KEYDOWN:
@@ -160,8 +166,6 @@ class Camera:
 
     # позиционировать камеру на объекте target
     def update(self, target):
-        print(target.rect.x + target.rect.w // 2)
-        print(target.rect.y + target.rect.h // 2)
         self.dx = -(target.rect.x + target.rect.w // 2 - WINDOW_WIDTH // 2)
         self.dy = -(target.rect.y + target.rect.h // 2 - WINDOW_HEIGHT // 2)
 
@@ -170,7 +174,14 @@ def main():
     bg = pygame.Color('black')
 
     player, level_x, level_y = generate_level(load_level('level1.dat'))
+
     camera = Camera()
+    # изменяем ракурс камеры
+    camera.update(player)
+    # обновляем положение всех спрайтов
+    for sprite in all_sprites:
+        camera.apply(sprite)
+
     playing = False
     running = True
     while running:
@@ -187,13 +198,13 @@ def main():
                         # обновляем положение всех спрайтов
                         for sprite in all_sprites:
                             camera.apply(sprite)
+        clock.tick(FPS)
         screen.fill(bg)
         if playing:
             all_sprites.draw(screen)
             player_group.draw(screen)
         else:
             start_screen(screen)
-        clock.tick(FPS)
         pygame.display.flip()
     pygame.display.quit()
 
