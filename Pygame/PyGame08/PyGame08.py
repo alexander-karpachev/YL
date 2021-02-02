e49254f985033bf37f239cf758395a0972c4c08e
Alexander Karpachev <alexander.karpachev@live.ru>
2021-02-02 23:46:07 +1100

    PyGame 8 - добавлены звездочки

--- Pygame/PyGame08/PyGame08.py
+++ Pygame/PyGame08/PyGame08.py
@@ -1,9 +1,79 @@
+import random
+
 import pygame, sys, os
 
 
 WINDOW_SIZE = WINDOW_WIDTH, WINDOW_HEIGHT = 800, 600
 FPS = 30
 all_sprites = pygame.sprite.Group()
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+# для отслеживания улетевших частиц
+# удобно использовать пересечение прямоугольников
+screen_rect = (0, 0, WINDOW_WIDTH, WINDOW_HEIGHT)
+GRAVITY = 1
+
+
+class Particle(pygame.sprite.Sprite):
+    # сгенерируем частицы разного размера
+    fire = [load_image("star.png")]
+    for scale in (5, 10, 20):
+        fire.append(pygame.transform.scale(fire[0], (scale, scale)))
+
+    def __init__(self, pos, dx, dy):
+        super().__init__(all_sprites)
+        self.image = random.choice(self.fire)
+        self.rect = self.image.get_rect()
+
+        # у каждой частицы своя скорость — это вектор
+        self.velocity = [dx, dy]
+        # и свои координаты
+        self.rect.x, self.rect.y = pos
+
+        # гравитация будет одинаковой (значение константы)
+        self.gravity = GRAVITY
+
+    def update(self):
+        # применяем гравитационный эффект:
+        # движение с ускорением под действием гравитации
+        self.velocity[1] += self.gravity
+        # перемещаем частицу
+        self.rect.x += self.velocity[0]
+        self.rect.y += self.velocity[1]
+        # убиваем, если частица ушла за экран
+        if not self.rect.colliderect(screen_rect):
+            self.kill()
+
+
+def create_particles(position):
+    # количество создаваемых частиц
+    particle_count = 20
+    # возможные скорости
+    numbers = range(-5, 6)
+    for _ in range(particle_count):
+        Particle(position, random.choice(numbers), random.choice(numbers))
 
 
 class AnimatedSprite(pygame.sprite.Sprite):
@@ -29,28 +99,6 @@ class AnimatedSprite(pygame.sprite.Sprite):
         self.image = self.frames[self.cur_frame]
 
 
-def load_image(name, colorkey=None):
-    fullname = os.path.join('data', name)
-    # если файл не существует, то выходим
-    if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найден")
-        sys.exit()
-    image = pygame.image.load(fullname)
-    if colorkey is not None:
-        image = image.convert()
-        if colorkey == -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey)
-    else:
-        image = image.convert_alpha()
-    return image
-
-
-def terminate():
-    pygame.quit()
-    sys.exit()
-
-
 def main():
     pygame.init()
     clock = pygame.time.Clock()
@@ -71,7 +119,12 @@ def main():
         if dragon_cnt > 2:
             dragon_cnt = 0
             dragon.update()
+            particle_x = random.randint(100, WINDOW_WIDTH-100)
+            particle_y = random.randint(100, WINDOW_HEIGHT-100)
+            create_particles((particle_x, particle_y))
         dragon_cnt += 1
+
+
 
         screen.fill(bg)
         all_sprites.draw(screen)
