9d69ea5c40efdf8a035a8f485847369166ed9e68
Alexander Karpachev <alexander.karpachev@live.ru>
2021-02-19 23:32:20 +1100

    Алгоритмы - сортировка Шелла

--- Algorithms/sorts/shell.py
+++ Algorithms/sorts/shell.py
@@ -1,22 +1,38 @@
+def quicksort(arr, left, right):
+    if left >= right:
+        return
+    center = part(arr, left, right)
+    quicksort(arr, left, center - 1)
+    quicksort(arr, center + 1, right)
+
+
+def part(arr, left, right):
+    i = left - 1
+    p = arr[right]
+    for j in range(left, right+1):
+        if a[j] <= p:
+            i += 1
+            a[i], a[j] = a[j], a[i]
+    return i
+
+
+
 
 def insertion_sort(array):
     for i in range(len(array)):
         x = array[i]
         j = i - 1
-        print(f'step:{i}, x={x}, j={j}')
-        print('       ', array)
         while j >= 0 and array[j] > x:
-            print(f'copy: array[{j+1}]=array[{j}], {array[j + 1]}->{array[j]}')
             array[j + 1] = array[j]  # "сдвиг" вправо
-            print(f'copy:  ', array)
-            j = j - 1
+            j -= 1
         array[j + 1] = x  # Вставка x в отсортированную часть
-        print('       ', array)
-        i = i + 1
 
 
 a = [10, 0, 1, 9, 3, 4, 5, 7, 6]
 
+#print(a)
+#insertion_sort(a)
+#print(a)
 print(a)
-insertion_sort(a)
+quicksort(a, 0, len(a)-1)
 print(a)
